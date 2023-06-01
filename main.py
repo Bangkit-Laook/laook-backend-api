a34f56e67092163e7ccec1073832200e76d0ab78
AldiNFitrah <aldifitrahx@gmail.com>
2023-06-01 12:36:27 +0700

    Add response model for all endpoints

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from fastapi import FastAPI, UploadFile, File
 from random import randint
+from pydantic import BaseModel
+from typing import List
 # import ml_model
 
 
 app = FastAPI()
 
 
-@app.post("/recognize_ingredients")
+class RecognizeIngredientsRequest(BaseModel):
+    image: UploadFile
+
+
+class RecognizeIngredientsResponse(BaseModel):
+    ingredients: List[str]
+
+
+class SuggestMenusRequest(BaseModel):
+    ingredients: List[str]
+
+
+class Menu(BaseModel):
+    name: str
+    description: str
+    image_url: str
+    ingredients: List[str]
+    steps: List[str]
+
+
+class SuggestMenusResponse(BaseModel):
+    menus: List[Menu]
+
+
+@app.post("/recognize_ingredients", response_model=RecognizeIngredientsResponse)
 async def recognize_ingredients(image: UploadFile = File(...)):
     # Save the uploaded image temporarily
     with open("temp_image.jpg", "wb") as temp_image:
@@ -14,15 +40,16 @@ async def recognize_ingredients(image: UploadFile = File(...)):
 
     # Pass the image to the machine learning model for ingredient recognition
     # recognized_ingredients = ml_model.recognize_ingredients("temp_image.jpg")
-    recognized_ingredients = [f"ing{i}" for i in range(20)]
+    recognized_ingredients = [f"ing{i}" for i in range(randint(2, 20))]
 
     # Return the list of recognized ingredients
     return {"ingredients": recognized_ingredients}
 
 
-@app.post("/suggest_menus")
-async def suggest_menus(ingredients: list):
-    print(ingredients)
+@app.post("/suggest_menus", response_model=SuggestMenusResponse)
+async def suggest_menus(request: SuggestMenusRequest):
+    ingredients = request.ingredients
+
     # Pass the ingredients to the machine learning model for menu suggestions
     # suggested_menus = ml_model.suggest_menus(ingredients)
     suggested_menus = [
@@ -32,7 +59,7 @@ async def suggest_menus(ingredients: list):
             "image_url": "https://picsum.photos/400/300",
             "ingredients": [f"ing{i}_{j}" for j in range(randint(4, 20))],
             "steps": [
-                f"This is step {j} in making menu{i}. You have to follow this religiously no matter what. Please follow the instructions clearly or else you will suffer."
+                {"step": f"This is step {j} in making menu{i}. You have to follow this religiously no matter what. Please follow the instructions clearly or else you will suffer."}
                 for j in range(randint(5, 15))
             ],
         } for i in range(randint(0, 10))
@@ -40,6 +67,3 @@ async def suggest_menus(ingredients: list):
 
     # Return the list of suggested menus
     return {"menus": suggested_menus}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
