d282f411a5957607a8ff3a6f4060ee175baa4b0a
AldiNFitrah <aldifitrahx@gmail.com>
2023-06-06 10:54:40 +0700

    Consider input ingredients in suggest menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import randint
+import random
+
 from typing import List
 
 from fastapi import FastAPI, UploadFile, File
@@ -49,7 +50,7 @@ async def recognize_ingredients(image: UploadFile = File(...)):
 
     # Pass the image to the machine learning model for ingredient recognition
     # recognized_ingredients = ml_model.recognize_ingredients("temp_image.jpg")
-    recognized_ingredients = [f"ing{i}" for i in range(randint(3, 20))]
+    recognized_ingredients = [f"ing{i}" for i in range(random.randint(3, 20))]
 
     # Return the list of recognized ingredients
     return {"ingredients": recognized_ingredients}
@@ -58,6 +59,7 @@ async def recognize_ingredients(image: UploadFile = File(...)):
 @app.post("/suggest_menus", response_model=SuggestMenusResponse)
 async def suggest_menus(request: SuggestMenusRequest):
     ingredients = request.ingredients
+    print(random.choice(ingredients))
 
     # Pass the ingredients to the machine learning model for menu suggestions
     # suggested_menus = ml_model.suggest_menus(ingredients)
@@ -66,12 +68,12 @@ async def suggest_menus(request: SuggestMenusRequest):
             "name": f"menu{i}",
             "description": f"Ini deskripsi untuk menu {i}. Menu ini memiliki ciri khas tersendiri dibanding menu-menu lain. Menu yang satu ini juga jadi favorit karena prosesnya mudah untuk dilakukan",
             "image_url": "https://picsum.photos/400/300",
-            "ingredients": [f"ing_{j}" for j in range(randint(4, 20))],
+            "ingredients": [*set(random.choice(ingredients) for _ in range(random.randint(1, 10)))] + [f"other ingredient {j}" for j in range(random.randint(0, 10))],
             "steps": [
                 f"This is step {j} in making menu{i}. You have to follow this religiously no matter what. Please follow the instructions clearly or else you will suffer."
-                for j in range(randint(5, 15))
+                for j in range(random.randint(5, 15))
             ],
-        } for i in range(randint(0, 10))
+        } for i in range(random.randint(0, 10))
     ]
 
     # Return the list of suggested menus
